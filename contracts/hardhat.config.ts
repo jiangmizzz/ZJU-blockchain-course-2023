6e2646af3e74a0a77b2e020a0384f2ca25fc92d2
“xubiying” <2094197975@qq.com>
2023-10-26 11:30:53 +0800

    init: initialize the program develop environment

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -4,13 +4,14 @@ import "@nomicfoundation/hardhat-toolbox";
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
   networks: {
+    hardhat: {},
     ganache: {
       // rpc url, change it according to your ganache configuration
-      url: 'http://localhost:8545',
+      url: "http://127.0.0.1:8545",
       // the private key of signers, change it according to your ganache user
       accounts: [
-        '0x172a483415c5e119f1ea336d44646572ac34a088e7bc2884f8e1f83da4cb304a'
-      ]
+        "0x14572a8cf5213770c0bfb997c34e959e07dac15d270c5099746fb48a364aafab",
+      ],
     },
   },
 };
