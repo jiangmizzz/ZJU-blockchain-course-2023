179a541e883cabc07a20ca93644f9fb1400310af
“xubiying” <2094197975@qq.com>
2023-11-09 00:25:05 +0800

    chore: renew dependencies

--- contracts/contracts/BorrowYourCar.sol
+++ contracts/contracts/BorrowYourCar.sol
@@ -3,16 +3,19 @@ pragma solidity ^0.8.20;
 
 // Uncomment the line to use openzeppelin/ERC721
 // You can use this dependency directly because it has been installed by TA already
-// import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 // Uncomment this line to use console.log
-// import "hardhat/console.sol";
+import "hardhat/console.sol";
 
 contract BorrowYourCar {
-
     // use a event if you want
     // to represent time you can choose block.timestamp
-    event CarBorrowed(uint32 carTokenId, address borrower, uint256 startTime, uint256 duration);
+    event CarBorrowed(
+        uint32 carTokenId,
+        address borrower,
+        uint256 startTime,
+        uint256 duration
+    );
 
     // maybe you need a struct to store car information
     struct Car {
@@ -22,6 +25,7 @@ contract BorrowYourCar {
     }
 
     mapping(uint256 => Car) public cars; // A map from car index to its information
+
     // ...
     // TODO add any variables if you want
 
@@ -29,7 +33,7 @@ contract BorrowYourCar {
         // maybe you need a constructor
     }
 
-    function helloworld() pure external returns(string memory) {
+    function helloworld() external pure returns (string memory) {
         return "hello world";
     }
 
